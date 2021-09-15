283ec4fbf3da8cd52278235fd41be0640cf33d2a
Tianbao Liang <tianbao.liang@nxp.com>
2021-09-15 16:44:12 +0800

    secure: Upload scripts which are used to generate signed/encrypted image for RT600
    Signed-off-by: Tianbao Liang <tianbao.liang@nxp.com>

---
+++ target/evkmimxrt600/secure/program_ocotp.bat
@@ -0,0 +1,92 @@
+::***************************************************************************
+:: This file show how to burnning ocotp by blhost
+:: 
+::***************************************************************************
+
+::***************************************************************************
+:: Set utility path
+:: User need to update the path according MCUX_Provi position!!!!!!!!!!
+::***************************************************************************
+@echo off
+SET "PATH=C:\nxp\MCUX_Provi_v3\bin\tools\blhost\win;%PATH%"
+SET com_port=COM22,115200
+@echo on
+
+::***************************************************************************
+:: Program RKT hash(RKTH) value
+:: User need to update the RKTH value below!!!!!!!!!!
+:: Below is an example of RKTH
+:: RKTH: 8b8123193c27489fe835e104be046187dbc5507c310de41b469e68d5842decc0
+::***************************************************************************
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x78 1923818b
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x79 9f48273c
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x7A 04e135e8
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x7B 876104be
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x7C 7c50c5db
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x7D 1be40d31
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x7E d5689e46
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x7F c0ec2d84
+
+
+::***************************************************************************
+:: Enable Secure boot
+::***************************************************************************
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x60 900000
+
+::***************************************************************************
+:: Configure Quad/Octal-SPI flash devices settings for RT685-EVK
+::***************************************************************************
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x61 314000
+
+::***************************************************************************
+:: Read RKTH value back
+::***************************************************************************
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x78
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x79
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x7a
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x7b
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x7c
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x7d
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x7e
+blhost -p %com_port% -t 15000 -- efuse-read-once 0x7f
+
+
+::!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+:: Below is the configuretion for image decryption
+:: It doesn't need to configure these parameters if user don't encrypt image
+:: i.e.
+:: OTP_MASTER_KEY: 000102030405060708090a0b0c0d0e0f00112233445566778899aabbccddeeff
+:: OTFAD KEK seed: 504d1862298daed564f26abf7feb723a
+:: OTFAD KEK:      0102030405060708090a0b0c0d0e0f00
+::!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+::***************************************************************************
+:: Program OTP_MASTER_KEY
+:: User need to update the key value below!!!!!!!!!!
+:: i.e. 00112233445566778899aabbccddeeff
+::***************************************************************************
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x70 ccddeeff
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x71 8899aabb
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x72 44556677
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x73 00112233
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x74 0c0d0e0f
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x75 08090a0b
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x76 04050607
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x77 00010203
+
+::***************************************************************************
+:: Configure OTFAD SEED
+:: OTFAD KEK seed example: 504d1862298daed564f26abf7feb723a
+::***************************************************************************
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x6c 62184d50
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x6d d5ae8d29
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x6e bf6af264
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x6f 3a72eb7f
+
+::***************************************************************************
+:: OTFAD_CFG
+::***************************************************************************
+::blhost -p %com_port% -t 15000 -- efuse-program-once 0x6a 00001000
+
+
+pause
---
+++ target/evkmimxrt600/secure/sign_enc_sbl_app.bat
@@ -0,0 +1,127 @@
+::***************************************************************************
+:: This file show how to generate signed SBL and application
+:: 
+::***************************************************************************
+
+::***************************************************************************
+:: Set utility path
+:: User need to update the path according MCUX_Provi position!!!!!!!!!!
+::***************************************************************************
+@echo off
+SET "PATH=C:\nxp\MCUX_Provi_v3\bin\tools\elftosb\win;%PATH%"
+SET "PATH=C:\nxp\MCUX_Provi_v3\bin\tools\blhost\win;%PATH%"
+SET "PATH=C:\nxp\MCUX_Provi_v3\bin\tools\image_enc\win;%PATH%"
+SET imgtool_path=..\..\..\component\secure\mcuboot\scripts
+
+SET com_port=COM24,115200
+
+SET user_kek=kek=0102030405060708090a0b0c0d0e0f00
+SET initial_otfad_arg=otfad_arg=[00112233445566778899aabbccddeeff,0020406001030507,0x08001000,0x1000],[00112233445566778899aabbccddeeff,0020406001030507,0x08101000,0x5000]
+SET sfw1_otfad_arg=otfad_arg=[00112233445566778899aabbccddeeff,0020406001030507,0x08101000,0x5000]
+
+@echo on
+
+::***************************************************************************
+:: Configure signing method RSA2048, ECDSAP256 or ROM_API
+::***************************************************************************
+set signing_type=ROM_API
+
+set mcu_header_size=0x400
+
+::***************************************************************************
+:: Prepare signed SBL image
+::***************************************************************************
+
+if not exist ".\sbl.bin" (
+    echo Can't find file sbl.bin
+    pause
+)
+
+elftosb -V -f rt6xx -J .\signed_sbl_xip.json
+
+:: File sbl should start at 0x08001000. Insert 0x1000 bytes at the beginning of sbl for tool image_enc.exe
+python img_helper.py paddingimage --pad-size 0x1000 --input .\sbl_signed.bin --output .\sbl_padding.bin
+
+image_enc.exe hw_eng=otfad ifile=.\sbl_padding.bin ofile=.\sbl_enc.bin base_addr=0x08000000 %user_kek% %initial_otfad_arg%
+
+:: Extract keyblob
+python img_helper.py extract-keycontext --type otfad --enc_image .\sbl_enc.bin --output .\keyblob_initial.bin
+
+:: Delete the first 4K bytes data. We program area KeyBlob and Flash Config Block later
+python img_helper.py deleteheader --image .\sbl_enc.bin --size 4096
+
+::***************************************************************************
+:: Prepare signed SFW image
+::***************************************************************************
+if %signing_type% == ROM_API (
+
+    if not exist ".\sfw.bin" (
+        echo Can't find file sfw.bin
+        pause
+    )
+
+    :: Sign image
+    elftosb -V -f rt6xx -J .\signed_sfw_xip.json
+    
+    :: Insert 0x400 bytes at the beginning of sfw for tool image_enc.exe
+    python img_helper.py paddingimage --pad-size %mcu_header_size% --input .\sfw_1_signed.bin --output .\sfw_1_padding.bin
+
+    image_enc.exe hw_eng=otfad ifile=.\sfw_1_padding.bin ofile=.\sfw_1_enc.bin base_addr=0x08100000 %user_kek% %sfw1_otfad_arg%
+    
+    python img_helper.py extract-keycontext --type otfad --enc_image .\sfw_1_enc.bin --output .\sfw_1_keyblob.bin
+    
+    :: Add mcuboot header and tlvs
+    python %imgtool_path%\imgtool.py create --align 4  --version "1.0"  --header-size %mcu_header_size% --pad-header --slot-size 0x100000 --key-info .\sfw_1_keyblob.bin .\sfw_1_signed.bin .\sfw_1_bootheader.bin
+
+    :: Merge mcuboot header and tlvs into encrypted image 
+    python img_helper.py merge --header-size %mcu_header_size% --sign-image .\sfw_1_bootheader.bin --enc-image .\sfw_1_enc.bin
+
+) else (
+
+    if not exist ".\sfw.bin" (
+        echo Can't find file sfw.bin
+        pause
+    )
+
+    :: File sfw start at 0x400. Insert 0x400 bytes at the beginning of sfw for tool image_enc.exe
+    python img_helper.py paddingimage --pad-size %mcu_header_size% --input .\sfw.bin --output .\sfw_1.bin
+
+    image_enc hw_eng=otfad ifile=.\sfw_1.bin ofile=.\sfw_1_enc.bin base_addr=0x08100000 %user_kek% %sfw1_otfad_arg%
+
+    :: Extract the key context, then insert it into mcuboot header
+    python img_helper.py extract-keycontext --type otfad --enc_image .\sfw_1_enc.bin --output .\sfw_1_keyblob.bin
+
+    :: Sign plain image with imgtool
+    if %signing_type% == RSA2048 (
+        python %imgtool_path%\imgtool.py sign --key %imgtool_path%\sign-rsa2048-priv.pem --align 4 --version "1.0" --header-size %mcu_header_size% --pad-header --slot-size 0x100000 --max-sectors 32 --key-info .\sfw_1_keyblob.bin  .\sfw.bin .\sfw_1_sign.bin
+    ) else (
+        python %imgtool_path%\imgtool.py sign --key %imgtool_path%\sign-ecdsap256-priv.pem --align 4 --version "1.0" --header-size %mcu_header_size% --pad-header --slot-size 0x100000 --max-sectors 32 --key-info .\sfw_1_keyblob.bin  .\sfw.bin .\sfw_1_sign.bin
+    )
+
+    :: Merge mcuboot header and tlvs into encrypted image 
+    python img_helper.py merge --header-size %mcu_header_size% --sign-image .\sfw_1_sign.bin --enc-image .\sfw_1_enc.bin
+)
+
+
+@echo sbl_enc.bin and sfw_1_enc.bin are the final image, press any key to download them
+pause
+@echo on
+
+::***************************************************************************
+:: Download image
+::***************************************************************************
+blhost  -p %com_port% -t 15000 -- get-property 1
+blhost  -p %com_port% -t 15000 -- fill-memory 0x1c000 4 0xC1503057
+blhost  -p %com_port% -t 15000 -- fill-memory 0x1c004 4 0x20000014
+blhost  -p %com_port% -t 15000 -- configure-memory 0x9 0x1c000
+blhost  -p %com_port% -t 15000 -- flash-erase-region 0x08000000 0x10000
+blhost  -p %com_port% -t 15000 -- fill-memory 0x1d000 4 0xf000000f
+blhost  -p %com_port% -t 15000 -- configure-memory 0x9 0x1d000
+
+blhost  -p %com_port% -t 15000 -- write-memory 0x08000000 .\keyblob_initial.bin
+blhost  -p %com_port% -t 15000 -- write-memory 0x08001000 .\sbl_enc.bin
+
+blhost  -p %com_port% -t 15000 -- flash-erase-region 0x08100000 0x40000
+blhost  -p %com_port% -t 15000 -- write-memory 0x08100000 .\sfw_1_enc.bin
+
+pause
